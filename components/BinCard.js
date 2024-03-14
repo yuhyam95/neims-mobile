aa35b791e47e00a983e9e8d62b4799820790b998
Yusuf Habu <yuhyam95@gmail.com>
2024-03-14 22:38:19 +0100

    Created BinCard

---
+++ components/BinCard.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
+import moment from 'moment'; // Make sure to install the moment library
+
+function BinCard({ product } ) {
+
+  return (
+    <>
+      <View>
+        <Text style={{fontSize: 20, marginLeft: 15}}>
+         BinCard
+        </Text>
+      </View>
+      
+      <View style={styles.container}>
+        <View style={styles.tableHeader}>
+          <Text style={styles.columnHeader}>Date</Text>
+          <Text style={styles.columnHeader}>SIV/SRV</Text>
+          <Text style={styles.columnHeader}>Movement</Text>
+          <Text style={styles.columnHeader}>Quantity</Text>
+          <Text style={styles.columnHeader}>Balance</Text>
+          <Text style={styles.columnHeader}>Signature</Text>
+        </View>
+
+        <View>
+          {product?.map((product, index) => (
+            <BinCardRows
+                key={index}
+                date={moment(product?.createdAt).format("MMMM Do YYYY")}
+                number={product?.srvnumber || product?.sivnumber} 
+                movement={product?.movement}
+                quantity={product?.quantity}
+                balance={product?.balance}
+                signature={product?.signature}
+          />
+          ))}
+        </View>
+      </View>
+      </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#ddd'
+  },
+  tableHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    paddingBottom: 15,
+  },
+  columnHeader: {
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  tableFooter: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  pagination: {
+    flexDirection: 'row',
+  },
+  paginationButton: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  paginationText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
+
+export default MyTable;
---
+++ components/BinCardRows.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+function BinCardRows({ name, quantity, reason, date, category, expiryDate }) {
+  
+
+  return (
+    <View style={styles.row}>
+      <View style={styles.cell}>
+        <Text style={styles.text}>{date}</Text>
+      </View>
+     
+      <View style={styles.cell}>
+        <Text style={styles.text}>{number}</Text>
+      </View>
+      
+      <View style={styles.cell}>
+        <View style={[styles.reasonBox, { backgroundColor: reason === 'restock' ? '#BAF2E0' : '#FBD8D8' }]}>
+          <Text style={{ color: reason === 'restock' ? '#2FD197' : '#EB4547', fontSize: 13 }}>
+            {reason === 'restock' ? 'RESTOCK' : 'DISTRIBUTION'}
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.cell}>
+        <View style={styles.quantityBox}>
+          <Text style={styles.text}>{quantity}</Text>
+        </View>
+      </View>
+
+      <View style={styles.cell}><Text style={styles.text}>{balance}</Text></View>
+      <View style={styles.cell}><Text style={styles.text}>{signature}</Text></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text:{
+    fontSize: 13
+  },
+  row: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    paddingVertical: 10,
+  },
+  cell: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  quantityBox: {
+    borderWidth: 0.5,
+    borderRadius: 10,
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  reasonBox: {
+    borderWidth: 0.5,
+    borderRadius: 10,
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'teal',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+  },
+});
+
+export default BinCardRows;
